ad3ec19d2467ab4025091b888823d7db873539e4
poncheska <43468296+poncheska@users.noreply.github.com>
2019-07-05 16:15:10 +0200

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -5,8 +5,8 @@ import android.service.voice.AlwaysOnHotwordDetector
 object Utils{
     fun parseFullName(fullName:String?):Pair<String?,String?>{
         val parts:List<String>? = fullName?.split(" ")
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val firstName = if(parts?.getOrNull(0)!="")parts?.getOrNull(0) else null
+        val lastName = if(parts?.getOrNull(1)!="")parts?.getOrNull(0) else null
         return firstName to lastName
     }
 
