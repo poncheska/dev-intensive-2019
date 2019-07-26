42fc69fc9e08a7726e16794d69a6f89710701332
poncheska <43468296+poncheska@users.noreply.github.com>
2019-07-26 21:58:31 +0200

    homework 4

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,28 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Bender
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("M_MainActivity","onCreate")
+
+
+    }
+
+}
